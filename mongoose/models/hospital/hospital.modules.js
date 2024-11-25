93c348f7c81e672bbc8dd4cbe5bda5980026d7c2
i-OmSharma <sharmaom1201@gmail.com>
2024-11-25 18:23:22 +0530

    Data Modeling Hospital

--- mongoose/models/hospital/hospital.modules.js
+++ mongoose/models/hospital/hospital.modules.js
@@ -1,5 +1,29 @@
 import mongoose from "mongoose";
 
-const hospitalSchema = new mongoose.Schema({}, {timestamps: true})
+const hospitalSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            requireed: true
+        },
+        address: {
+            type: String,
+            requireed: true
+        },
+        city: {
+            type: String,
+            requireed: true
+        },
+        pinCode: {
+            type: String,
+            requireed: true
+        },
+        specalizedIn: [
+            {
+                type: String,
+                required: true 
+            }
+        ]
+    }, {timestamps: true})
 
 export const Hospital = mongoose.model("Hospital", hospitalSchema) 
