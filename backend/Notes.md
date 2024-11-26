760e74d06dbf4127c96b5af245f864793d0cfc38
i-OmSharma <sharmaom1201@gmail.com>
2024-11-26 21:03:02 +0530

    MongoDB connection error

--- backend/Notes.md
+++ backend/Notes.md
@@ -1,5 +1,8 @@
-Dependency -a dependency is required for an application to run in production.
+* Dependency -a dependency is required for an application to run in production.
     eg: npm i ___
     vs
-devDependemcy -  while a devdependency is only needed for development and testing.
+*  devDependemcy -  while a devdependency is only needed for development and testing.
     eg: npm i -D ___
+
+
+* Always use try-catch and keep of async-await in data base 
---
+++ backend/src/index.js
@@ -0,0 +1,62 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import e from "express";
+const app = e()
+
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGO_URI}/${vtube}`)
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${precess.env.PORT}`);
+            
+        } )
+
+    } catch (error) {
+        console.log("ERROR: ", error)
+        throw error
+        
+    }
+})()
+
+*/
