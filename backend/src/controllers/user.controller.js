09862eeb50eb3daea5ef55dec28cf3692b8372bc
i-OmSharma <sharmaom1201@gmail.com>
2024-12-13 23:29:05 +0530

    updated Contollers

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,9 +1,18 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "Ok "
-    })
+    //get user detail from frontend
+    // validation
+    //  heck if user already exists: email
+    // upload them to cloudinary: avatar
+    //create user object - create entry in db
+    // remove pass & refresh token field from response
+    // check for user creation
+    // return res
+
+    const {fullname, email, userName, password} = req.body
+    console.log("email:", email);
+    
 })
 
 export default registerUser
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,19 +3,14 @@ import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import { app } from "./app.js"
 
-
-
 dotenv.config({
     path:'./.env'
 })
 
-
-
-
 connectDB()
 .then(() => {
     app.on("error", (error) => {
-        console.log("ERROR: ", error);
+        console.log("ERROR: ", error)
         throw error
     })
     app.listen(process.env.PORT || 8000, () => {(
